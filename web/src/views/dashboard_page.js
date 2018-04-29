64fa9a0224ea3aebcf7614d6e3da2998be68a39d
HAREESH Polla <hpolla@torovm1.vnet.ibm.com>
2018-04-29 16:35:01 -0600

    GQL data fetching with Axios

--- web/src/views/dashboard_page.js
+++ web/src/views/dashboard_page.js
@@ -9,51 +9,11 @@ import GalleryAlbumActions from './Gallery/gallery_album_actions';
 class DashboardPage extends React.Component {
   componentWillMount() {
     this.setState({ message: '' });
-    this.fetchMessage();
-  }
-
-  fetchMessage() {
-    // const { getAccessToken } = this.props.auth;
-    // console.log("Token:",getAccessToken());
-    // const headers = { 'Authorization': `Bearer ${getAccessToken()}`}
-
-    // axiosAPI.get(`/private`)
-    //   .then(response => this.setState({ message: response.data.message }))
-    //   .catch(error => this.setState({ message: error.message }));
-
-    let query = `{
-      god(id:"5ae4f2f111445ed7e9eff33e"){
-        id
-        title
-        image
-        items
-      }
-      godByTitle(title:"Sri Rama"){
-        title
-        image
-        items
-      }
-      gods{
-        id
-        title
-        image
-        items
-      }
-    }`;
-    axiosGQL.post('/', {
-      "operationName": null,
-      "variables": null,
-      query
-    })
-      .then(response => this.setState({ message: response.data }))
-      .catch(error => this.setState({ message: error.message }));
-
   }
 
   render() {
     return (
       <div>
-        Dashboard :{JSON.stringify(this.state.message)}
 
         <div id="album">
           <GalleryAlbumActions />
--- web/src/views/gods_page.js
+++ web/src/views/gods_page.js
@@ -1,47 +1,14 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 
-import axios from '../axios';
+import { axiosGQL } from '../axios';
 
 import CardGod from './Gallery/card_god';
 
-const GODS = [
-  {
-    title: "Sri Ganesh",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 3
-  },
-  {
-    title: "Sri Rama",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 0
-  },
-  {
-    title: "Sri Gayathri Mata",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 5
-  },
-  {
-    title: "Sri Sai Ram",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 5
-  },
-  {
-    title: "Sri Lakshmi matha Ji Ki Jai",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 5
-  },
-  {
-    title: "Sri Saraswathi Mata",
-    image: "https://source.unsplash.com/random/150x120",
-    items: 5
-  }
-];
-
 class GodsPage extends React.Component {
+  state = { gods: [] };
 
   componentWillMount() {
-    this.setState({ gods: GODS });
     this.fetchGods();
   }
 
@@ -49,6 +16,13 @@ class GodsPage extends React.Component {
     // axios.get(`/gods`)
     //   .then(response => this.setState({ gods: response.data.gods }))
     //   .catch(error => this.setState({ gods: [] }));
+    axiosGQL.post('/', {
+      "operationName": null,
+      "variables": null,
+      "query": "{gods{id title image items}}"
+    })
+      .then(response => this.setState({ gods: response.data.data.gods }))
+      .catch(error => this.setState({ message: error.message }));
   }
 
   render() {
@@ -56,7 +30,7 @@ class GodsPage extends React.Component {
       <div style={{ marginTop: "4.5em" }}>
         <Grid>
           {
-            this.state.gods.map((god, idx) => (
+            this.state.gods && this.state.gods.map((god, idx) => (
               <Grid.Column computer={4} tablet={6} mobile={8} stretched textAlign="center">
                 <CardGod  {...god} key={idx} />
               </Grid.Column>
