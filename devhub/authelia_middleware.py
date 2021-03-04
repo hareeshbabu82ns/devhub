e8a30c1327673e0dd5164b7b451f1b20e3bbd946
Hareesh Polla <hpolla@atb.com>
2021-03-03 23:01:12 -0700

    drone docker deploy
    ansible deploy with traefik flags

--- devhub/authelia_middleware.py
+++ devhub/authelia_middleware.py
@@ -22,12 +22,14 @@ class AutheliaMiddleware:
 
         user = request.session.get(AUTHELIA_USER_KEY)
         if not user:
-            if request.headers.get('Secretkey', '') == SECRET_KEY:
-                logger.warn('switching to admin user')
-                user = {'id': 'admin', 'display_name': 'Admin'}
-            else:
-                logger.warn('fetching auth user')
-                user = self.fetch_authelia_user(request)
+            user = {'id': 'admin', 'display_name': 'Admin'}
+            # disbling authelia to find better authentication
+            # if request.headers.get('Secretkey', '') == SECRET_KEY:
+            #     logger.warn('switching to admin user')
+            #     user = {'id': 'admin', 'display_name': 'Admin'}
+            # else:
+            #     logger.warn('fetching auth user')
+            #     user = self.fetch_authelia_user(request)
             if user and user['id']:
                 self.user = user
             request.session[AUTHELIA_USER_KEY] = self.user
