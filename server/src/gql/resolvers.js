09a1a4e04c7c8f93465a9e7d643b78cf4bf2d194
Hareesh Polla <hareeshbabu82@gmail.com>
2022-04-22 19:50:29 -0600

    sanscript utils api routing

--- server/src/gql/resolvers.js
+++ server/src/gql/resolvers.js
@@ -1,110 +1,135 @@
-const UserResolvers = require( './resolvers/user' );
-const EntityResolvers = require( './resolvers/entity' );
-const EntityTypeResolvers = require( './resolvers/entity_type' );
-const LanguageResolvers = require( './resolvers/language' );
-const { getRequestedFields } = require( './resolvers/utils' );
+const UserResolvers = require( './resolvers/user' )
+const EntityResolvers = require( './resolvers/entity' )
+const EntityTypeResolvers = require( './resolvers/entity_type' )
+const LanguageResolvers = require( './resolvers/language' )
+const SanscriptUtilsResolvers = require( './resolvers/sanscript_utils' )
+const { getRequestedFields } = require( './resolvers/utils' )
 
 const resolvers = {
   Query: {
     version: ( parent, args ) => {
-      return "v1";
+      return "v1"
     },
 
     // User 
     me: UserResolvers.me,
     users: async ( parent, args, context, info ) => {
-      const requestedFields = getRequestedFields( info );
-      return UserResolvers.read( args, requestedFields );
+      const requestedFields = getRequestedFields( info )
+      return UserResolvers.read( args, requestedFields )
     },
 
     // Entity 
     entities: async ( parent, args, context, info ) => {
-      const requestedFields = getRequestedFields( info );
-      return EntityResolvers.read( args, requestedFields );
+      const requestedFields = getRequestedFields( info )
+      return EntityResolvers.read( args, requestedFields )
     },
 
     // Entity Type
     entityTypes: async ( parent, args, context, info ) => {
-      const requestedFields = getRequestedFields( info );
-      return EntityTypeResolvers.read( args, requestedFields );
+      const requestedFields = getRequestedFields( info )
+      return EntityTypeResolvers.read( args, requestedFields )
     },
 
     // Language Type
     languages: async ( parent, args, context, info ) => {
-      const requestedFields = getRequestedFields( info );
-      return LanguageResolvers.read( args, requestedFields );
+      const requestedFields = getRequestedFields( info )
+      return LanguageResolvers.read( args, requestedFields )
     },
+
+    // Sanscript Utils
+    dictionarySearch: async ( parent, args, context, info ) => {
+      const requestedFields = getRequestedFields( info )
+      return SanscriptUtilsResolvers.dictionarySearch( args, requestedFields )
+    },
+
+    dictionarySearchById: async ( parent, args, context, info ) => {
+      const requestedFields = getRequestedFields( info )
+      return SanscriptUtilsResolvers.dictionarySearchById( args, requestedFields )
+    },
+
+    splits: async ( parent, args, context, info ) => {
+      return SanscriptUtilsResolvers.splits( args )
+    },
+
+    joins: async ( parent, args, context, info ) => {
+      return SanscriptUtilsResolvers.joins( args )
+    },
+
+    transliterate: async ( parent, args, context, info ) => {
+      return SanscriptUtilsResolvers.transliterate( args )
+    },
+
   },
 
   Mutation: {
     // initialization logic
     init: async ( parent, args ) => {
-      await UserResolvers.init();
-      await EntityTypeResolvers.init();
-      await LanguageResolvers.init();
-      await EntityResolvers.init();
-      return "data successfully initialized";
+      await UserResolvers.init()
+      await EntityTypeResolvers.init()
+      await LanguageResolvers.init()
+      await EntityResolvers.init()
+      return "data successfully initialized"
     },
 
     // User
     createUser: async ( parent, args ) => {
-      const res = await UserResolvers.create( args.withData );
-      return res;
+      const res = await UserResolvers.create( args.withData )
+      return res
     },
     updateUser: async ( parent, args ) => {
-      const res = await UserResolvers.update( args.id, args.withData );
-      return res;
+      const res = await UserResolvers.update( args.id, args.withData )
+      return res
     },
     deleteUser: async ( parent, args ) => {
-      const res = await UserResolvers.delete( args.id );
-      return res;
+      const res = await UserResolvers.delete( args.id )
+      return res
     },
 
     // Entity
     createEntity: async ( parent, args ) => {
-      const res = await EntityResolvers.create( args.withData );
-      return res;
+      const res = await EntityResolvers.create( args.withData )
+      return res
     },
     updateEntity: async ( parent, args ) => {
-      const res = await EntityResolvers.update( args.id, args.withData );
-      return res;
+      const res = await EntityResolvers.update( args.id, args.withData )
+      return res
     },
     deleteEntity: async ( parent, args ) => {
-      const res = await EntityResolvers.delete( args.id );
-      return res;
+      const res = await EntityResolvers.delete( args.id )
+      return res
     },
 
     // Entity Type
     createEntityType: async ( parent, args ) => {
-      const res = await EntityTypeResolvers.create( args.withData );
-      return res;
+      const res = await EntityTypeResolvers.create( args.withData )
+      return res
     },
     updateEntityType: async ( parent, args ) => {
-      const res = await EntityTypeResolvers.update( args.id, args.withData );
-      return res;
+      const res = await EntityTypeResolvers.update( args.id, args.withData )
+      return res
     },
     deleteEntityType: async ( parent, args ) => {
-      const res = await EntityTypeResolvers.delete( args.id );
-      return res;
+      const res = await EntityTypeResolvers.delete( args.id )
+      return res
     },
 
     // Language Type
     createLanguage: async ( parent, args ) => {
-      const res = await LanguageResolvers.create( args.withData );
-      return res;
+      const res = await LanguageResolvers.create( args.withData )
+      return res
     },
     updateLanguage: async ( parent, args ) => {
-      const res = await LanguageResolvers.update( args.id, args.withData );
-      return res;
+      const res = await LanguageResolvers.update( args.id, args.withData )
+      return res
     },
     deleteLanguage: async ( parent, args ) => {
-      const res = await LanguageResolvers.delete( args.id );
-      return res;
+      const res = await LanguageResolvers.delete( args.id )
+      return res
     },
   },
 
   ...EntityResolvers.type,
   ...EntityTypeResolvers.type,
-};
+}
 
 module.exports = resolvers
---
+++ server/src/gql/resolvers/sanscript_utils.js
@@ -0,0 +1,72 @@
+const axios = require( 'axios' )
+
+const sanscriptUtilsAPI = axios.create( {
+  baseURL: process.env.SANSCRIPT_UTILS_URL,
+  timeout: 5000,
+  headers: { 'X-Sanscript-Client': 'DevHubJS' }
+} )
+
+module.exports = {
+
+  dictionarySearch: async ( { searchWith }, requestedFields ) => {
+    const query = `
+      query search($searchWith: DictionarySearchInput!){
+        dictionarySearch(searchWith:$searchWith){
+          ${requestedFields.join( ' ' )}
+        }
+      }
+    `
+    const res = await sanscriptUtilsAPI.post( '', { query, variables: { searchWith } } )
+    // console.log( res?.data )
+    return res?.data?.data?.dictionarySearch
+  },
+
+  dictionarySearchById: async ( { id }, requestedFields ) => {
+    const query = `
+      query search($id: ID!){
+        dictionarySearchById(id:$id){
+          ${requestedFields.join( ' ' )}
+        }
+      }
+    `
+    const res = await sanscriptUtilsAPI.post( '', { query, variables: { id } } )
+    // console.log( res?.data )
+    return res?.data?.data?.dictionarySearchById
+  },
+
+  splits: async ( { text, schemeFrom, schemeTo, strictIO, limit } ) => {
+    const query = `
+      query splits($text: String!, $schemeFrom: SanscriptScheme, 
+                  $schemeTo: SanscriptScheme, $strictIO: Boolean, $limit: Int){
+        splits(text:$text, schemeFrom:$schemeFrom, 
+          schemeTo: $schemeTo, strictIO: $strictIO, limit: $limit)
+      }
+    `
+    const res = await sanscriptUtilsAPI.post( '', { query, variables: { text, schemeFrom, schemeTo, strictIO, limit } } )
+    return res?.data?.data?.splits
+  },
+
+  joins: async ( { words, schemeFrom, schemeTo, strictIO } ) => {
+    const query = `
+      query joins($words: [String!]!, $schemeFrom: SanscriptScheme, 
+                  $schemeTo: SanscriptScheme, $strictIO: Boolean){
+        joins(words:$words, schemeFrom:$schemeFrom, 
+          schemeTo: $schemeTo, strictIO: $strictIO)
+      }
+    `
+    const res = await sanscriptUtilsAPI.post( '', { query, variables: { words, schemeFrom, schemeTo, strictIO } } )
+    return res?.data?.data?.joins
+  },
+
+  transliterate: async ( { text, schemeFrom, schemeTo } ) => {
+    const query = `
+      query transliterate($text: String!, $schemeFrom: SanscriptScheme, 
+                  $schemeTo: SanscriptScheme){
+        transliterate(text:$text, schemeFrom:$schemeFrom, schemeTo: $schemeTo)
+      }
+    `
+    const res = await sanscriptUtilsAPI.post( '', { query, variables: { text, schemeFrom, schemeTo } } )
+    return res?.data?.data?.transliterate
+  },
+
+}
